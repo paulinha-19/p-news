e52a849360bacd475a107d5585b32c96c3e1ff85
paulinha-19 <paulafabianasoares@gmail.com>
2025-06-07 19:22:07 -0300

    [update] TabLayout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,5 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
-import { Pressable, Image } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
@@ -14,14 +13,10 @@ export default function TabLayout() {
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerTintColor: Colors.light.text,
         headerStyle: { backgroundColor: Colors.light.tint },
-        headerRight: () => (
-          <Pressable onPress={() => { }} style={{ marginRight: 16 }}>
-            <Ionicons name="search-outline" size={20} color={Colors.dark.text} />
-          </Pressable>
-        ),
+        headerShown: false,
       }}>
       <Tabs.Screen
-        name="home/index"
+        name="home"
         options={{
           title: '',
           tabBarIcon: ({ color, size, focused }) => (
@@ -29,13 +24,6 @@ export default function TabLayout() {
               name={focused ? 'home' : 'home-outline'}
               color={color}
               size={size}
-            />
-          ),
-          headerLeft: () => (
-            <Image
-              source={require('../../assets/images/logo-1.png')}
-              style={{ width: 60, height: 60, marginLeft: 16 }}
-              resizeMode="contain"
             />
           )
         }}
