64c2d33963b6c3ccaa80e927c7c1e70047978928
paulinha-19 <paulafabianasoares@gmail.com>
2025-06-07 12:53:17 -0300

    [add] card index

---
+++ components/common/Card/index.tsx
@@ -0,0 +1,15 @@
+import CardSummary from "./CardSummary";
+import CardFooter from "./CardFooter";
+import CardMedia from "./CardMedia";
+import CardRoot from "./CardRoot";
+import CardTitle from "./CardTitle";
+import CardDetails from "./CardDetails";
+
+export const Card = {
+    Root: CardRoot,
+    Media: CardMedia,
+    Title: CardTitle,
+    Details: CardDetails,
+    Summary: CardSummary,
+    Footer: CardFooter,
+};
