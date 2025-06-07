7148b0259c9cddd3a076298a10afa5bc785f3030
paulinha-19 <paulafabianasoares@gmail.com>
2025-06-07 19:19:42 -0300

    [update] type Articles

--- types/articles.ts
+++ types/articles.ts
@@ -1,9 +1,10 @@
 export type Articles = {
     title: string;
     description: string;
-    publishedAt: string;
+    content: string;
     url: string;
     image: string;
+    publishedAt: string;
     source: {
         name: string;
         url: string;
