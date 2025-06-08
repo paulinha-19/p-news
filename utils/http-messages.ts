0df7fcf9b2393eb69b0e7bb905b3bbbcf3ea2667
paulinha-19 <paulafabianasoares@gmail.com>
2025-06-08 03:13:34 -0300

    [fix] message of httpErrorMessages

--- utils/http-messages.ts
+++ utils/http-messages.ts
@@ -1,13 +1,13 @@
 export const httpErrorMessages: Record<number, string> = {
     400: 'Requisição inválida.',
     401: 'Você precisa inserir uma API_KEY válida',
-    403: 'Você não tem permissão para acessar este recurso.',
+    403: 'Você atingiu sua cota diária, o próximo reset será às 00:00 UTC',
     404: 'Conteúdo não encontrado.',
     408: 'Tempo de resposta esgotado.',
-    429: 'Muitas requisições. Tente novamente mais tarde.',
-    500: 'Erro de servidor. Tente novamente mais tarde.',
+    429: 'Você fez mais pedidos por segundo do que o permitido',
+    500: 'Tivemos um problema com o nosso servidor. Tente novamente mais tarde',
     502: 'Bad Gateway.',
-    503: 'Serviço temporariamente indisponível.',
+    503: 'Estamos temporariamente offline para manutenção. Por favor, tente novamente mais tarde.',
     504: 'Servidor demorou para responder.',
 };
 
